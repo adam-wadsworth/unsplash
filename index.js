341100f2bddaa8a78efc86b4574d63e91a6407be
Adam Wadsworth <adamkwadsworth@Adams-MBP.lan>
2020-01-16 23:00:42 +0000

    Unsplash - get photo data

---
+++ index.js
@@ -0,0 +1,33 @@
+const unsplashAxios = require('./unsplashAxios');
+
+class Unsplash {
+  constructor(path) {
+    return unsplashAxios(path)
+  }
+}
+
+class GetPhoto {
+  constructor() {
+    const unsplash = new Unsplash('/photos/PgHc0Ka1E0A');
+    unsplash.then((response) => {
+      console.log(response)
+    }).catch((response) => {
+      console.log(response);
+    });
+  }
+}
+
+class GetCollection {
+  constructor() {
+    const unsplash = new Unsplash('/collections/62890');
+    unsplash.then((response) => {
+      console.log(response)
+    }).catch((response) => {
+      console.log(response);
+    });
+  }
+}
+
+
+const getPhoto = new GetPhoto();
+const getCollection = new GetCollection();
