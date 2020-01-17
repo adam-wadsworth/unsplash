dd04123916592a8d182466d3f6eb72890aaa2cfa
Blue Mantis <bluemantis@Blues-iMac.Hitronhub.home>
2020-01-17 08:47:55 +0000

    Class rewrite

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 const unsplashAxios = require('./unsplashAxios');
 
 class Unsplash {
-  constructor(path) {
-    return unsplashAxios(path)
-  }
-}
 
-class GetPhoto {
   constructor() {
-    const unsplash = new Unsplash('/photos/PgHc0Ka1E0A');
-    unsplash.then((response) => {
+    this.unsplash = null;
+  }
+
+  getPhoto(id){
+    this.unsplash = unsplashAxios( `/photos/${id}`);
+    this.unsplash.then((response) => {
       console.log(response)
     }).catch((response) => {
       console.log(response);
     });
   }
-}
 
-class GetCollection {
-  constructor() {
-    const unsplash = new Unsplash('/collections/62890/photos');
-    unsplash.then((response) => {
+  getCollection(id){
+    this.unsplash = unsplashAxios(`/collections/${id}/photos`);
+    this.unsplash.then((response) => {
       console.log(response)
     }).catch((response) => {
       console.log(response);
     });
   }
+
 }
 
-const getPhoto = new GetPhoto();
-const getCollection = new GetCollection();
+myUnsplash = new Unsplash();
+myUnsplash.getPhoto('PgHc0Ka1E0A');
+myUnsplash.getCollection('62890');
